a6b3fc20c09bd920f04c2a944ec1701d8f847520
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-04-14 18:50:35 -0400

    Update validate.js

--- js/validate.js
+++ js/validate.js
@@ -11,29 +11,22 @@ function validate(event) {
   lat = parseFloat(lat.value);
   long = parseFloat(long.value);
 
-  if (
-    (lat > 90 || lat < -90 || isNaN(lat)) &&
-    (long > 180 || long < -180 || isNaN(long))
-  ) {
-    document.querySelector("#lat-warning").innerText =
-      "* must be a valid Latitude (-90 to 90)";
+  if(lat > 90 || lat < -90 || isNaN(lat)){
+    document.querySelector("#lat-warning").innerText = "* must be a valid Latitude (-90 to 90)";
     event.preventDefault();
-  } else {
-    if (lat > 90 || lat < -90 || isNaN(lat)) {
-      document.querySelector("#lat-warning").innerText =
-        "* must be a valid Latitude (-90 to 90)";
-      event.preventDefault();
-    } else {
-      document.querySelector("#lat-warning").innerText = "*";
-    }
-    if (long > 180 || long < -180 || isNaN(long)) {
-      document.querySelector("#long-warning").innerText =
-        "* must be a valid Longitude (-180 to 180)";
-      event.preventDefault();
-    } else {
-      document.querySelector("#long-warning").innerText = "*";
-      flag = true;
-    }
+  }
+  else{
+    document.querySelector("#lat-warning").innerText = "*";
+    flag = true;
+  }
+
+  if(long > 180 || long < -180 || isNaN(long)){
+    document.querySelector("#long-warning").innerText = "* must be a valid Longitude (-180 to 180)";
+       event.preventDefault();
+  }
+  else{
+    document.querySelector("#long-warning").innerText = "*";
+    flag = true;
   }
   console.log(
     "TODO - validate the longitude, latitude values before submitting"
