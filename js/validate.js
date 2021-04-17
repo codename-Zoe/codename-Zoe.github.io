9afc42eceb17311116252b5f5905dcb36a419cd6
Zoe <78044798+codename-Zoe@users.noreply.github.com>
2021-04-17 12:14:02 -0400

    Add files via upload

--- js/validate.js
+++ js/validate.js
@@ -5,6 +5,7 @@ function validate(event) {
   // invalid, show the appropriate error message in the form, and stop the
   // form from being submitted. If both values are valid, allow the form to be
   // submitted.
+
   let lat = document.querySelector("#lat");
   let long = document.querySelector("#long");
   let flag = false;
@@ -34,8 +35,32 @@ function validate(event) {
       document.querySelector("#long-warning").innerText = "*";
       flag = true;
     }
-    return flag;
   }
+
+  // display the error message at once
+
+  // if (lat > 90 || lat < -90 || isNaN(lat)) {
+  //   document.querySelector("#lat-warning").innerText =
+  //     "* must be a valid Latitude (-90 to 90)";
+  //   event.preventDefault();
+  // } else {
+  //   document.querySelector("#lat-warning").innerText = "*";
+  //   flag = true;
+  // }
+
+  // if (long > 180 || long < -180 || isNaN(long)) {
+  //   document.querySelector("#long-warning").innerText =
+  //     "* must be a valid Longitude (-180 to 180)";
+  //   event.preventDefault();
+  // } else {
+  //   document.querySelector("#long-warning").innerText = "*";
+  //   flag = true;
+  // }
+  console.log(
+    "TODO - validate the longitude, latitude values before submitting"
+  );
+  return flag;
+}
 
 // Wait for the window to load, then set up the submit event handler for the form.
 window.onload = function () {
